45308c5a05ed22310ab56554291288908ca1cea6
Lucas Lee <llzqingdao2012@gmail.com>
2021-11-17 22:28:14 +0800

    feat: 更新接口类型

--- typescript/integrated-webpack/src/interface.ts
+++ typescript/integrated-webpack/src/interface.ts
@@ -1,22 +1,106 @@
 interface IUser {
-  id: number;
+  readonly recordId: number;
   name: string;
+  age?: number;
+  [key: string]: any;
 }
 
-interface Result {
-  data: IUser[];
+interface IResult {
+  users: IUser[];
 }
 
-interface StringArray {
+const render = (result: IResult) =>
+  result.users.forEach((user) => {
+    console.log("render user", user);
+    if (user.age) {
+      console.log("age", user.age);
+      user.recordId = 1;
+    }
+  });
+
+const result = {
+  users: [
+    {
+      recordId: 1,
+      name: "张三",
+    },
+    {
+      recordId: 2,
+      name: "李四",
+    },
+  ],
+};
+render(result);
+
+const result1 = {
+  users: [
+    {
+      recordId: 1,
+      name: "张三",
+      age: 10,
+    },
+    {
+      recordId: 2,
+      name: "李四",
+    },
+  ],
+};
+
+// result能够完全符合接口IResult的定义
+render(result1);
+
+render({
+  users: [
+    {
+      recordId: 1,
+      name: "张三",
+      age: 10,
+    },
+    {
+      recordId: 2,
+      name: "李四",
+    },
+  ],
+});
+
+render({
+  users: [
+    {
+      recordId: 1,
+      name: "张三",
+      age: 10,
+    },
+    {
+      recordId: 2,
+      name: "李四",
+    },
+  ],
+} as IResult);
+
+render(<IResult>{
+  users: [
+    {
+      recordId: 1,
+      name: "张三",
+      age: 10,
+    },
+    {
+      recordId: 2,
+      name: "李四",
+    },
+  ],
+});
+
+interface StringUsernames {
   [index: number]: string;
 }
 
 // 用任意的数字去索引StringArray 都会返回string
-let chars: StringArray = ["A", "B"];
+const usernames: StringUsernames = ["张三", "李四"];
 
 interface Names {
-  [x: string]: string;
-  x: number; // 这样就不能申明number类型了。
+  [key: string]: string;
+  [index: number]: number;
 }
 
 // 2种索引可以混用，但是一定要确保返回是一种类型，即，是一种子类型。但是可以使用any进行类型扩展。
