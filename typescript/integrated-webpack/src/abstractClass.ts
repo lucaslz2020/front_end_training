321cc0beb5b4b527dde18a15490aa4918df486f5
Lucas Lee <llzqingdao2012@gmail.com>
2021-11-18 23:00:52 +0800

    feat: 抽象类

--- typescript/integrated-webpack/src/abstractClass.ts
+++ typescript/integrated-webpack/src/abstractClass.ts
@@ -1,5 +1,46 @@
-abstract class Animal {}
+abstract class Animal {
+  eat() {}
+
+  abstract run(): void;
+}
 
 // let animal = new Animal();
 
-class Dog extends Animal {}
+class Dog extends Animal {
+  run(): void {}
+}
+
+const dog = new Dog();
+dog.eat();
+
+class Cat extends Animal {
+  run(): void {}
+}
+
+let cat = new Cat();
+
+let animals: Animal[] = [dog, cat];
+
+animals.forEach((animal) => {
+  animal.run();
+});
+
+class WorkFlow {
+  step1() {
+    return this;
+  }
+  step2() {
+    return this;
+  }
+}
+
+new WorkFlow().step1().step2();
+
+// 多态 this可以是子类也可以是父类
+class PurchaseWorkFlow extends WorkFlow {
+  next() {
+    return this;
+  }
+}
+
+new PurchaseWorkFlow().next().step1().next().step2();
