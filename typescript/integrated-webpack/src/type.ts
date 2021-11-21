f9db12051bf94b15cc456730de060d60e0b0b637
Lucas Lee <llzqingdao2012@gmail.com>
2021-11-22 07:09:10 +0800

    refactor: 完善类型

--- typescript/integrated-webpack/src/type.ts
+++ typescript/integrated-webpack/src/type.ts
@@ -2,13 +2,32 @@ const hasWorking: boolean = true;
 const hasWorking1: boolean | undefined = undefined;
 const hasWorking2: boolean | null = null;
 
-const age: number = 20;
+let age: number = 20;
 const age1: number | undefined = undefined;
 const age2: number | null = null;
+const age3 = Number(20);
+const age4 = new Number(20);
+const age5 = 0.1;
+const age6 = 0b1010;
+const age7 = 0o774;
+const age8 = 0x00d;
+
+let age9: bigint = 20n;
+age9 = age;
+age9 = age3;
 
 const username: string = "lucas";
 const username1: string | undefined = undefined;
 const username2: string | null = null;
+const username3: string = String("lucas");
+const username4 = new String("lucas");
+const username5: string = `my name is ${username}`;
+
+String("lucas") === String("lucas");
+new String("lucas") === new String("lucas");
+String("lucas") === new String("lucas");
+String("lucas") === "lucas";
+new String("lucas") === "lucas";
 
 const children: number[] = [1, 2];
 const children1: (number | string)[] = [1, 2];
@@ -28,6 +47,11 @@ user.username = "bob";
 const symbol1: symbol = Symbol("symbol1");
 const symbol2: symbol = Symbol("symbol1");
 
+let any: any;
+any = 1;
+any = [];
+any = () => {};
+
 let error = () => {
   throw new Error("error");
 };
@@ -35,3 +59,15 @@ let error = () => {
 let endless = () => {
   while (true) {}
 };
+
+// unknown
+
+{
+  let unknown1: unknown;
+  let num: number = unknown1;
+  let any: any = unknown1;
+
+  if (typeof num === "number") {
+    num.toFixed();
+  }
+}
