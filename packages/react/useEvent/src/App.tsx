d875b3960d89aa4e28a947d0dc77b75c20be39f0
Lucas Lee <llzqingdao2012@gmail.com>
2022-05-15 20:13:35 +0800

    feat: useEvent

---
+++ packages/react/useEvent/src/App.tsx
@@ -0,0 +1,33 @@
+import { Motivation1 } from "./Motivation1";
+import { Motivation2 } from "./Motivation2";
+import { Motivation3 } from "./Motivation3";
+import { Motivation4 } from "./Motivation4";
+import { Motivation5 } from "./Motivation5";
+import { Motivation6 } from "./Motivation6";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Motivation1 />
+      </div>
+      <div>
+        <Motivation2 />
+      </div>
+      <div>
+        <Motivation2 />
+      </div>
+      <div>
+        <Motivation4 />
+      </div>
+      <div>
+        <Motivation5 />
+      </div>
+      <div>
+        <Motivation6 />
+      </div>
+    </div>
+  );
+}
+
+export default App;
