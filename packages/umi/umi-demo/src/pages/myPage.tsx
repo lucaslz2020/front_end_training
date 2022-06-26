55c77e72c571294db64cbc137fe0936d3a7f5b37
Lucas Lee <llzqingdao2012@gmail.com>
2022-06-26 23:46:00 +0800

    fix: umi ahooks useRequest集成

--- packages/umi/umi-demo/src/pages/myPage.tsx
+++ packages/umi/umi-demo/src/pages/myPage.tsx
@@ -1,10 +1,37 @@
+import { useRequest } from 'ahooks';
+import Mock from 'mockjs';
 import React from 'react';
 import styles from './myPage.less';
 
+function getUsername(): Promise<string> {
+  return new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(Mock.mock('@name'));
+    }, 3000);
+  });
+}
+
 export default function Page() {
+  const { data, error, loading } = useRequest(getUsername);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+  if (loading) {
+    return <div>loading...</div>;
+  }
   return (
     <div>
       <h1 className={styles.title}>Page myPage</h1>
     </div>
   );
 }
+
+const Page2 = () => {
+  const { run, runAsync } = useRequest(getUsername, {
+    manual: true,
+    onSuccess: (result, params) => {
+      console.log('result', result, params);
+    },
+  });
+};
