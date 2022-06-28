650b3d0410f3e849664641ce240b66ceb8ef182d
Lucas Lee <llzqingdao2012@gmail.com>
2022-06-28 08:26:19 +0800

    fix: 集成@alita/plugins keepalive tabsLayout

--- packages/umi/umi-demo/config/config.ts
+++ packages/umi/umi-demo/config/config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
   plugins: [
     require.resolve('@umijs/plugins/dist/model'),
     require.resolve('@umijs/plugins/dist/antd'),
+    require.resolve('@alita/plugins/dist/keepalive'),
+    require.resolve('@alita/plugins/dist/tabs-layout'),
   ],
   model: {}, // 必须配置model
   antd: {}, // 必须要有这个，不然antd的样式不会生效。
+  keepalive: [/./],
+  tabsLayout: {},
 });
